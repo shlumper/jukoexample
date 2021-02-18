a701c8695fe5de7ade0e8bea95fbaf8e1cf72fcb
mushon4 <mushon4@gmail.com>
2021-02-18 21:52:15 +0200

    cool

--- generate_fibo.py
+++ generate_fibo.py
@@ -1,23 +1,29 @@
-{
+import json
+import random
+
+
+tests = {
 	"FPGA-u250": {
 		"team": "devops",
 		"git_repo": "https://github.com/shlumper/jukoexample.git",
 		"test_cmd_path": "/root/jukoexample",
 		"image": "deepcontainerregistry.azurecr.io/base_python_image:0.1",
 		"test_list": {
+         
 			"basic": {
 				"required_mem": "1024",
-				"test_commands": {
-					"fib10": "python calculate.py 10",
-					"fib20": "python calculate.py 20",
-					"fib50": "python calculate.py 50",
-					"fib200": "python calculate.py 200",
-					"fib300": "python calculate.py 300"
-				},
 				"artifact_paths": [
 					"/root/cl_cifar10/verif/common/tools/index.html"
-				]
+				],
+            "test_commands": {}
 			}
 		}
 	}
 }
+
+for number in range(100):
+   rand = random.randint(0,50)
+   tests["FPGA-u250"]["test_list"]["basic"]["test_commands"]["fib" + str(number)] = "python calculate.py " + str(rand)
+
+
+print(json.dumps(tests, indent=4, sort_keys=True))
