2b9ceec38fc1b5fa23e42e8e1094d50060d5ef73
mushon4 <mushon4@gmail.com>
2021-02-22 13:25:47 +0200

    dsad

--- base_ubuntu_image/build_docker_image.sh
+++ base_ubuntu_image/build_docker_image.sh
@@ -1,2 +1,1 @@
-docker build . -t deepcontainerregistry.azurecr.io/basic_python_image:latest
-#az acr login --name deepcontainerregistry.azurecr.io
+docker build . -t deepcontainerregistry.azurecr.io/basic_python_image:1.0
