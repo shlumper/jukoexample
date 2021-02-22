fdb4aaa7dcdabf4a0938dc93909c24ea434ecf07
mushon4 <mushon4@gmail.com>
2021-02-22 12:49:24 +0200

    moko

--- base_ubuntu_image/build_docker_image.sh
+++ base_ubuntu_image/build_docker_image.sh
@@ -1,2 +1,2 @@
-docker build . -t deepcontainerregistry.azurecr.io/basic_python_image:1.0
+docker build . -t deepcontainerregistry.azurecr.io/basic_python_image:latest
 #az acr login --name deepcontainerregistry.azurecr.io
