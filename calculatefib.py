c846a39dceba4a60eef848a40f9aab8e372c9eb0
mushon4 <mushon4@gmail.com>
2021-02-23 14:40:05 +0200

    fix

--- calculatefib.py
+++ calculatefib.py
@@ -1,7 +1,22 @@
-
 import sys
+nterms = int(sys.argv[1])
 
-from fibonacci import fibonacci
+# first two terms
+n1, n2 = 0, 1
+count = 0
 
-
-print("Fibonacci cacluated succesfuly: ",fibonacci(int(sys.argv[1])) )
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print("fib counter n: " + str(count) + "- equals = " + str(n1))
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
